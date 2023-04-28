476b07cc8c2cbb51c10e88b0e8b9998a4d24a5dd
Mayara Vass <mayara_mv93@outlook.com>
2023-04-28 01:16:39 -0300

    Yelp MVP

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :content, :rating, presence: true
   validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { within: 0..5 }
+  validates :content, :rating, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   # get 'reviews/new'
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
 
-  ####### parei no item "User Stories"!!
+  # get 'restaurants', to: 'restaurants#index'
+
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # post 'restaurants', to: 'restaurants#create'
+
+  # get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+  # post 'restaurants/:restaurant_id/reviews/new', to: 'restaurants#create'
+
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [ :new, :create ]
+  end
+
 end
